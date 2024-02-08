29098f7e0b80544c202fee40e1f9d1abf1a5f4d1
Debapriyo Das <debapriyo00@gmail.com>
2024-02-08 09:45:05 +0530

    Time: 1 ms (99.90%), Space: 43.5 MB (44.27%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        //First i have to sort the array.
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        CalSubset(0, nums, ans, new ArrayList<>());
+        return ans;
+    }
+    
+     public static void CalSubset(int index, int arr[], List<List<Integer>>ans, List<Integer>list){
+         ans.add(new ArrayList<>(list));
+         
+         for(int i = index;i<arr.length;i++){
+             //This is if is't not be First and Duplicate then Continue.
+             if(i!=index && arr[i] == arr[i-1])continue;
+             list.add(arr[i]);
+             CalSubset(i+1, arr, ans, list);
+             list.remove(list.size()-1);
+         }
+         
+     }
+}
