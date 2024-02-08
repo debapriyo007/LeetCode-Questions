2ab1ab96ad1e591895b51ee0faab2621603341e2
Debapriyo Das <debapriyo00@gmail.com>
2024-02-08 13:06:24 +0530

    Time: 2 ms (99.84%), Space: 43.2 MB (56.45%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,23 +1,35 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        
-        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(candidates);
-        findCombinition(0,candidates, target, ans, new ArrayList<>());
+        List<List<Integer>> ans = new ArrayList<>();
+        
+        find(0, target,candidates, ans, new ArrayList<>());
         return ans;
+        
     }
     
-    public static void findCombinition(int indx, int arr[], int target,List<List<Integer>> ans, List<Integer> list){
+    // Find Combinition SUm Methord.
+    public static void  find(int index, int target, int arr[],List<List<Integer>> ans,List<Integer> list){
+        
+        //base case.
         if(target == 0){
             ans.add(new ArrayList<>(list));
             return;
         }
         
-        for(int i = indx;i<arr.length;i++){
-            if(i>indx && arr[i] == arr[i-1])continue;
-            if(arr[i]>target)break;
+        for(int i = index;i<arr.length;i++){
+            
+            //check for the repeated element.
+            //in question it say's that -- Each number in candidates may only be used once in the combination.
+            
+            //if repeated then i not gonna pick that, so continue..
+            if(i>index && arr[i] == arr[i-1] )continue; //i>index means the fist element i don't care duplicate or not.
+            if(arr[i]>target)break; //menas  arr[i] value is greater then i gonna break don't pick anything. don't excute anything.
+            
+            //if i pick.
             list.add(arr[i]);
-            findCombinition(i+1, arr, target-arr[i], ans , list);
+            
+            find(i+1, target - arr[i], arr, ans, list);
             list.remove(list.size()-1);
         }
     }
