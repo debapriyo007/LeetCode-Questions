c4901e778228788e95e9839cf14c2f94f7e70c14
Debapriyo Das <debapriyo00@gmail.com>
2024-02-09 12:09:21 +0530

    Time: 1 ms (95.85%), Space: 44.3 MB (75.35%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,13 +1,23 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
+        
+        
+       /*
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
         boolean frq[] = new boolean[nums.length];
         findPermutation(nums, ans, list, frq);
         
+        */
+        
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        Find(0, nums, ans);
+        
         return ans;
         
     }
+    /*
     public static void findPermutation(int arr[], List<List<Integer>> ans, List<Integer> list, boolean frq[]){
         //base case.
         if(list.size() == arr.length){
@@ -31,4 +41,32 @@ class Solution {
             }
         }
     }
+    */
+    
+    
+    public static void swap(int i, int j , int arr[]){
+        int tem = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tem;
+    }
+    
+    public static void Find(int index, int arr[],  List<List<Integer>> ans){
+        //base case.
+        if(index == arr.length){ //if my index size is reach length 
+            //creating a new list ans add that list into my ans list.
+           ArrayList<Integer> list = new ArrayList<>();
+            for(int i = 0;i<arr.length;i++){
+                list.add(arr[i]);
+            }
+            ans.add(new ArrayList<>(list)); //here add in ans list.
+            return;
+        }
+        
+        for(int i = index;i<arr.length;i++){
+            swap(i, index, arr); //swap loop varriable with curr position.
+            Find(index+1, arr, ans); //call for next ele.
+            swap(i, index, arr);
+        }
+    }
+    
 }
