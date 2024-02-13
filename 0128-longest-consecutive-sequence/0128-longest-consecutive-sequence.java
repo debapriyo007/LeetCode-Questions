7d66c67986c71a64e599af6206e8fe8fa7796be7
Debapriyo Das <debapriyo00@gmail.com>
2024-02-13 11:14:29 +0530

    Time: 1079 ms (24.58%), Space: 67 MB (29.65%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -7,17 +7,26 @@ class Solution {
             set.add(nums[i]);
         }
         
+        //declear my longest length.
         int longestLen = 0;
         
+        //Step 2: Iterate through the array.
         for(int i = 0;i<nums.length;i++){
+            // if my curr number -1 not present in the Set
             if(!set.contains(nums[i]-1)){
+                //store my currNumber.
                 int currNum = nums[i];
+                //also store my curr number length.
                 int currLen = 1;
+                
+                //My curr number next number is present in the Set 
+                // like my curr num is - 99 , so , if 100 is present in the Set
                 while(set.contains(currNum+1)){
-                    currNum +=1;
-                    currLen+=1;
+                    
+                    currNum +=1; //update 100 into 101 and so on.
+                    currLen+=1; // also upadte the length as well
                 }
-                 longestLen = Math.max(longestLen,currLen);
+                 longestLen = Math.max(longestLen,currLen); //compare with my currlen and my declear longest len.
             }
            
         }
