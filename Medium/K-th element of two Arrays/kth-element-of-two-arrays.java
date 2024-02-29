86b26d0f302f96740d07739339b7f206f9760eea
Debapriyo Das <debapriyo00@gmail.com>
2024-02-29 09:01:57 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+            
+            int n = Integer.parseInt(stt.nextToken());
+            int m = Integer.parseInt(stt.nextToken());
+            int k = Integer.parseInt(stt.nextToken());
+            int a[] = new int[(int)(n)];
+            int b[] = new int[(int)(m)];
+            
+            
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            String inputLine1[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < m; i++) {
+                b[i] = Integer.parseInt(inputLine1[i]);
+            }
+            
+            
+            Solution obj = new Solution();
+            System.out.println(obj.kthElement( a, b, n, m, k));
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    public long kthElement( int arr1[], int arr2[], int n, int m, int k) {
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        int i = 0;
+        int j = 0;
+        while(i<n && j<m){
+            if(arr1[i]<arr2[j]){
+                list.add(arr1[i++]);
+            }else{
+                list.add(arr2[j++]);
+            }
+        }
+        
+        while(i<n){
+          list.add(arr1[i++]);  
+        }
+        
+        while(j<m){
+          list.add(arr2[j++]);  
+        }
+        
+        return list.get(k-1);
+    }
+}
