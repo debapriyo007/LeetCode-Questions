fcbaf05b3194ab08a1d498f503b5bffb9fa1dab3
Debapriyo Das <debapriyo00@gmail.com>
2024-03-01 08:53:12 +0530

    Time: 11 ms (11.74%), Space: 45.9 MB (43.91%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        //Brute Froce 
+        //TC -> 0(N^2)
+        //SC -> 0(1)
+        
+        int n = matrix.length;
+        int m = matrix[0].length;
+        
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(matrix[i][j] == target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
