4af8069a6dc334eae656024137950314c8777f53
Debapriya Das <debapriyo00@gmail.com>
2024-03-05 11:35:25 +0530

    Time: 1 ms (99.86%), Space: 44.5 MB (70.21%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,31 +1,27 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        findCombination(0, target, candidates, ans, new ArrayList<>());
-        return ans;
-    }
-    
-    public static void findCombination(int index, int target, int arr[],List<List<Integer>> ans, List<Integer>list){
+    public static void Find(int index, int arr[], int target, List<List<Integer>>ans, List<Integer> ds){
         
-        
-        
-        //base.
+        //base case.
         if(index == arr.length){
             if(target == 0){
-                ans.add(new ArrayList<>(list));
+                ans.add(new ArrayList<>(ds));
             }
             return;
         }
         
-        
         if(arr[index]<= target){
-            list.add(arr[index]);
-            //pick condition.
-            findCombination(index, target- arr[index], arr, ans, list);
-            list.remove(list.size()-1);
+            //this is the pick condition.
+            ds.add(arr[index]);
+            Find(index, arr, target - arr[index], ans, ds); //decrement my target.
+            ds.remove(ds.size()-1);
         }
-        //Non pick condition.
-        findCombination(index+1, target, arr, ans, list);
+        //non - pick condition.
+        Find(index+1, arr, target, ans, ds);
+        
     }
-    
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Find(0,candidates,target, ans, new ArrayList<>());
+        return ans;
+    }
 }
