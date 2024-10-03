00b2321f40480849a8863f8e2648ac116bf5bfa5
Debapriya Das <debapriyo00@gmail.com>
2024-10-03 13:39:55 +0530

    Time: 25 ms (66.67%), Space: 56.5 MB (61.03%) - LeetHub

--- 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.java
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.java
@@ -16,20 +16,47 @@ class Solution {
         
         int currentMod = 0;
         
-        // Traverse the array and track prefix sums modulo p
+
         for (int i = 0; i < n; i++) {
             currentMod = (currentMod + nums[i]) % p;
             int targetMod = (currentMod - totalSum + p) % p;
             
-            // Check if we've seen the required mod before
             if (map.containsKey(targetMod)) {
                 minLength = Math.min(minLength, i - map.get(targetMod));
             }
-            
-            // Store the current prefix mod and index
+       
             map.put(currentMod, i);
         }
         
         return minLength == n ? -1 : minLength;
+                
+        
+//         long sum = 0;
+//         for(int ele : nums){
+//             sum+= ele;
+//         }
+        
+//         //if the sum is less than p, return -1
+//         if (sum < p) return -1;
+//         //if the whole sum is divided by p return 0.
+//         if(sum % p == 0)return 0;
+        
+//         int n = nums.length;
+//         int ans = Integer.MAX_VALUE; 
+        
+//         //remove subarray and check the rest of sum is devide by p or not..
+//         for(int i = 0;i<n;i++){
+//             int subArraySum = 0;
+//             for(int j = i;j<n;j++){
+//                 subArraySum+= nums[j];
+//                 if((sum - subArraySum)%p == 0){
+//                    ans = Math.min(ans, j-i+1); 
+//                 }
+//             }
+//         }
+//         return (ans == Integer.MAX_VALUE) ? -1 : ans;
+//      }
+    
+    
     }
 }
