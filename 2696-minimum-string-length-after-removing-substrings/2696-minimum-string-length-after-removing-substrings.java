78a904b815731d6916a5f055f5b24d0c2d236738
Debapriya Das <debapriyo00@gmail.com>
2024-10-07 14:25:42 +0530

    Time: 7 ms (36.18%), Space: 44.6 MB (24.59%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
@@ -6,26 +6,5 @@ class Solution {
             s = s.replace("CD", "");
         }
         return s.length();
-        
-        
-//         if(!s.contains("AB") || !s.contains("CD")){
-//             return s.length();
-//         }
-        
-//         String ans = recursion(s, "");
-//         return ans.length();
-    }
-    
-    String recursion(String s, String ans){
-        if(!s.contains("AB") || !s.contains("CD")){
-            return ans;
-        }
-         for(int i = 0;i<s.length();i++){
-            if(s.contains("AB") || s.contains("CD")){
-                 s = s.replace("AB", "");
-                 s = s.replace("CD", "");  
-            }
-        }
-        return recursion(ans, ans+s);
     }
 }
