1d0130bfb29b407ba8fd4768c9564b65111f5301
Debapriya Das <debapriyo00@gmail.com>
2024-10-08 13:43:10 +0530

    Time: 14 ms (76.82%), Space: 56 MB (10.37%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,16 +1,46 @@
 class Solution {
     public int minSwaps(String s) {
         
+        // int cnt = 0;
+        // for(char ch : s.toCharArray()){
+        //     if(ch == '['){
+        //         cnt++;
+        //     }else{
+        //         if(cnt > 0){
+        //             cnt --;
+        //         }
+        //     }
+        // }
+        // return (cnt+1)/2;
+            
+        char[]strArr = s.toCharArray();
+        int i = 0;
+        int j = s.length()-1;
         int cnt = 0;
-        for(char ch : s.toCharArray()){
-            if(ch == '['){
-                cnt++;
-            }else{
-                if(cnt > 0){
-                    cnt --;
-                }
+        int cntOpenBracket = 0;
+        while(i<j){
+            if (strArr[i] == '[') {
+                cntOpenBracket++; 
+            } else if (strArr[i] == ']') {
+                cntOpenBracket--;  
             }
+            
+            if(cntOpenBracket < 0){
+                while(i<j && strArr[j] != '['){
+                    j--;
+                }
+                swap(strArr, i, j);
+                cnt++;
+                cntOpenBracket = 1;
+                j--;
+            }
+            i++;
         }
-        return (cnt+1)/2;
+        return cnt;   
+    }
+    void swap(char[]arr, int x, int y){
+        char tem = arr[x];
+        arr[x] = arr[y];
+        arr[y] = tem;
     }
 }
