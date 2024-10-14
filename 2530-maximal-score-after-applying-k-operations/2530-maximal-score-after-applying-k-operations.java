1ca5cb75a86f70f581048ae765b5d750a63635e3
Debapriya Das <debapriyo00@gmail.com>
2024-10-14 11:27:25 +0530

    Time: 153 ms (53.08%), Space: 63 MB (49.76%) - LeetHub

---
+++ 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.java
@@ -0,0 +1,36 @@
+class Solution {
+    public long maxKelements(int[] nums, int k) {
+        int n = nums.length;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int i = 0 ;i<n ;i++){
+            pq.offer(nums[i]);
+        }
+        
+        long ans = 0;
+        while(k-- > 0){
+            int val = pq.poll();
+            ans+= val;
+            pq.offer((int)Math.ceil(val/3.0));
+        }
+        return ans;
+    }
+}
+
+//         int n = nums.length;
+//         long ans = 0;
+//         for(int i = 0;i<k;i++){
+//            //find the max ele index.
+//             int maxIndx = 0;
+//             for(int j = 1 ;j<n;j++){
+//                 if(nums[j] > nums[maxIndx]){
+//                     maxIndx = j;
+//                 }
+//             }
+            
+//             ans += nums[maxIndx];
+//             //update the maxIndex value.
+//             nums[maxIndx] = (int)Math.ceil(nums[maxIndx]/3.0);
+            
+//         }
+//         return ans;
