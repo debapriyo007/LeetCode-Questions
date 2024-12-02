fd3a71f7793bd933338def8bbbfe5b829bff07dd
Debapriya Das <debapriyo00@gmail.com>
2024-12-02 12:55:30 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (14.31%) - LeetHub

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
@@ -5,13 +5,18 @@ class Solution {
         //split base of space.
         String[]senAr = sentence.split(" ");
         // System.out.println("Split array is : " + Arrays.toString(senAr));
-        
+        int m = searchWord.length();
         for(int i = 0;i<senAr.length;i++){
             String currString = senAr[i];
             // System.out.println("Curr STRING : " + currString);
-            if(currString.startsWith(searchWord)){
-                return i+1;
-            }
+            // if(currString.startsWith(searchWord)){
+            //     return i+1;
+            // }
+            if(currString.length() >= m){
+                if(currString.substring(0, m).equals(searchWord)){
+                    return i+1;
+                }
+            } 
         }
         return -1;
     }
